638ac7235dd77a6f58897405a0953a382ba53954
vincenttwizere <v.twizere@alustudent.com>
2023-02-10 12:53:23 -0800

    Task 12

--- python-hello_world/102-magic_calculation.py
+++ python-hello_world/102-magic_calculation.py
@@ -1,8 +1,2 @@
-#!/usr/bin/python3
-def magic_calculation(a, b, c):
-    if a < b:
-        return c
-    elif c > b:
-        return a + b
-
-    return a * b - c
+def magic_calculation(a, b):
+    return (98 + a ** b)
