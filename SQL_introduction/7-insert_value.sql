752dcd673687a09f34632d1541b604cf65e47ec0
vincenttwizere <v.twizere@alustudent.com>
2023-03-29 16:21:48 -0700

    seven

--- SQL_introduction/7-insert_value.sql
+++ SQL_introduction/7-insert_value.sql
@@ -1,2 +1,3 @@
-INSERT INTO first_table (id, name)
-VALUES (89, 'Best School');
+-- Insert into a table
+-- Insert a row into a table
+INSERT INTO `first_table` (id, name) VALUES (89, "Best School");
