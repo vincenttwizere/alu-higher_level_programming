de6c62dabb51b7e07d98a9e667e46540e3b481b5
vincenttwizere <v.twizere@alustudent.com>
2023-04-05 19:02:30 +0000

    python-network_0 activities

---
+++ python-network_0/2-delete.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# a Bash script that sends a DELETE request to the URL passed as the first argument and displays the body of the response
+curl -s "$1" -X DELETE#!/bin/bash
+# a Bash script that sends a DELETE request to the URL passed as the first argument and displays the body of the response
+curl -s "$1" -X DELETE
---
+++ python-network_0/4-header.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# a Bash script that takes in a URL as an argument, sends a GET request to the URL, and displays the body of the response.
+curl -s "$1" -X GET -H "X-School-User-Id: 98"#!/bin/bash
+# a Bash script that takes in a URL as an argument, sends a GET request to the URL, and displays the body of the response.
+curl -s "$1" -X GET -H "X-School-User-Id: 98"
