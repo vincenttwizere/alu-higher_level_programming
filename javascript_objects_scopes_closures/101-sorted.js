c92ffb5ef006c6a389ecac6de4be405a8dcabbf6
vincenttwizere <v.twizere@alustudent.com>
2023-06-16 11:20:04 -0700

    Sorted occurences

--- javascript_objects_scopes_closures/101-sorted.js
+++ javascript_objects_scopes_closures/101-sorted.js
@@ -1,9 +1,13 @@
 #!/usr/bin/node
-exports.dict = {
-  89: 1,
-  90: 2,
-  91: 1,
-  92: 3,
-  93: 1,
-  94: 2
-};
+const dict = require('./101-data.js').dict;
+
+const newDict = {};
+
+Object.getOwnPropertyNames(dict).forEach(occurences => {
+  if (newDict[dict[occurences]] === undefined) {
+    newDict[dict[occurences]] = [occurences];
+  } else {
+    newDict[dict[occurences]].push(occurences);
+  }
+});
+console.log(newDict);
